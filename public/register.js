3a3f19f93389d9614a38c9e6f981f443b56c8145
musikloper <musikloper0601@gmail.com>
2021-09-17 17:09:24 +0900

    register.js 수정

--- public/register.js
+++ public/register.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
   
+  // 중복체크 버튼 클릭했을 때
   $('#idcheckbtn').click(function () {
+    // 작성하기
     const id = $('#id').val()
     console.log(id)
-
+a
     $.post("/idcheck", {
       id : id
     })
@@ -11,8 +13,12 @@ $(document).ready(function () {
       console.log(data)
       
       if (!data) {
+        // 작성하기
+
         alert('사용가능한 아이디입니다')  
       } else {
+        // 작성하기
+
         alert('이미 사용중인 아디입니다')
       }
     })
